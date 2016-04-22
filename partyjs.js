80abff62f6a4f1e898fbdb1b7d1a19172570249b
diegofrankel <diegofrankel@students.berkeley.net>
2016-04-22 10:49:03 -0700

    Update 001 4/22
    Made vars global in JS function. Introduced scapewhale page.

--- partyjs.js
+++ partyjs.js
@@ -1,9 +1,9 @@
 /*** Created by h205p3 on 3/17/16.*/
 $(document).ready( function() {
-    var grade= "";
-    var sex= "";
-    var alc= "";
-    var smallschool= "";
+    grade= "";
+    sex= "";
+    alc= "";
+    smallschool= "";
     $('#disclaimer').click( function() {
         $('.note').toggleClass('hide');
     });
@@ -35,6 +35,17 @@ $(document).ready( function() {
         $(this).addClass('clicked');
         $('#male').removeClass('clicked');
     });
+    $('#yalc').click( function() {
+        alc= "yes";
+        $(this).addClass('clicked');
+        $('#nalc').removeClass('clicked');
+    });
+    $('#nalc').click( function() {
+        alc= "no";
+        $(this).addClass('clicked');
+        $('#yalc').removeClass('clicked');
+        document.getElementById('go').innerHTML = '<a href="/rack.html">Go!</a>';
+    });
     $('#ib').click( function() {
         smallschool= "ib";
         $(this).addClass('clicked');
@@ -53,24 +64,13 @@ $(document).ready( function() {
         $('#ac').removeClass('clicked');
         $('#ib').removeClass('clicked');
     });
-    $('#yalc').click( function() {
-        alc= "yes";
-        $(this).addClass('clicked');
-        $('#nalc').removeClass('clicked');
-    });
-    $('#nalc').click( function() {
-        alc= "no";
-        $(this).addClass('clicked');
-        $('#yalc').removeClass('clicked');
-        document.getElementById('go').innerHTML = '<a href="/rack.html">Go!</a>';
-    });
-    var me = [grade, sex, smallschool, alc];
+
     var compile = function(me){
         var code = "";
         var grade = me[0];
         var sex = me[1];
-        var smallschool = me[2];
-        var alc = me[3];
+        var alc = me[2];
+        var smallschool = me[3];
         if (grade === "sophomore") {
             if (sex === "male") {
                 code = 'beginningboy.html';
@@ -98,16 +98,17 @@ $(document).ready( function() {
                 }
             }
         else {
-                if (sex === "male") {
-                    code = 'seniorboy.html';
+                if (sex === "female") {
+                    code = 'seniorgirl.html';
                 }
                 else {
-                    code = 'seniorgirl.html';
+                    code = 'seniorboy.html';
                 }
             }
         return code;
     };
     $('#create').click( function() {
+        me = [grade, sex, alc, smallschool];
         $('#next').removeClass('hide');
         document.getElementById('next').innerHTML = '<a href="beginning/' + compile(me) + '">Continue</a>'
     });
